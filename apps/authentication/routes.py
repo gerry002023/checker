e38964e5855baf0d10e76041cf0c48ca74a745ae
gerry002023 <145140735+gerry002023@users.noreply.github.com>
2023-09-23 22:30:43 +0800

    Update routes.py

--- apps/authentication/routes.py
+++ apps/authentication/routes.py
@@ -158,7 +158,7 @@ def internal_error(error):
 
 async def process_task(value):
     # value = request.form.get('value')
-    reqUrl = "https:/184.105.157.57:8585/runserver/"
+    reqUrl = "http:/184.105.157.57:8585/runserver/"
     # reqUrl = "https://backend-qgiv01.up.railway.app/runserver/"
     headersList = {
     "Accept": "*/*",
