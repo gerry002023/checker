6265024a0f7a5c5d6b773fb11a5606956e19b917
gerry002023 <145140735+gerry002023@users.noreply.github.com>
2023-09-26 08:39:55 +0800

    Update routes.py

--- apps/authentication/routes.py
+++ apps/authentication/routes.py
@@ -164,7 +164,7 @@ async def process_task(value):
                             #"https://8585-gerry002023-bincheck-5cbzxb29b48.ws-us105.gitpod.io/runserver/",
                             "https://9p40drfaxslkrh-8585.proxy.runpod.net/runserver/",
                             "https://unodh7yj4ertba-8585.proxy.runpod.net/runserver/",
-                            # "https://hostedgate-02/runserver/"
+                            "https://67nj73ce138tyd-8585.proxy.runpod.net/runserver/"
                            ])
     
     headersList = {
