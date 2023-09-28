cfa5ea413f1f0a255d67cf974a889443676bc713
The Beatless <thebeatless780@gmail.com>
2023-09-28 21:31:54 +0000

    a

--- apps/authentication/routes.py
+++ apps/authentication/routes.py
@@ -3,34 +3,37 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-from flask import render_template, redirect, request, url_for
+from flask import render_template, redirect, request, url_for, Blueprint
 from flask_login import (
     current_user,
     login_user,
     logout_user
 )
 from flask_dance.contrib.github import github
-# from check.checker import check_card
 from apps import db, login_manager
-from apps.authentication import blueprint
 from apps.authentication.forms import LoginForm, CreateAccountForm
 from apps.authentication.models import Users
-from flask import jsonify
 from apps.authentication.util import verify_pass
 import json
-import time
 import random
 import requests
-import threading
 import asyncio
-# import jsonify
+import os
 
-user_semaphores = {}
+blueprint = Blueprint('authentication_blueprint', __name__)
 
-def find_between( data, first, last ):
+def get_rotation_gates():
+    GATE_LIST = os.environ.get('GATE_LIST')
+    if GATE_LIST is None:
+        return 'gate-001.up.railway.app'
+    else:
+        GATE_LIST = [item.strip() for item in GATE_LIST.split(',')]
+        return random.choice(GATE_LIST)
+
+def find_between(data, first, last):
     try:
-        start = data.index( first ) + len( first )
-        end = data.index( last, start )
+        start = data.index(first) + len(first)
+        end = data.index(last, start)
         return data[start:end]
     except ValueError:
         return None
@@ -54,81 +57,51 @@ def login_github():
 def login():
     login_form = LoginForm(request.form)
     if 'login' in request.form:
-
-        # read form data
-        user_id  = request.form['username'] # we can have here username OR email
+        user_id = request.form['username']
         password = request.form['password']
-
-        # Locate user
         user = Users.find_by_username(user_id)
 
-        # if user not found
         if not user:
-
             user = Users.find_by_email(user_id)
+        
+        if not user:
+            return render_template('accounts/login.html', msg='Unknown User or Email', form=login_form)
 
-            if not user:
-                return render_template( 'accounts/login.html',
-                                        msg='Unknown User or Email',
-                                        form=login_form)
-
-        # Check the password
         if verify_pass(password, user.password):
-
             login_user(user)
             return redirect(url_for('authentication_blueprint.route_default'))
 
-        # Something (user or pass) is not ok
-        return render_template('accounts/login.html',
-                               msg='Wrong user or password',
-                               form=login_form)
-#     isallowed = Users.check_allowed(user_id)
-    if not current_user.is_authenticated:
-        return render_template('accounts/login.html',
-                               form=login_form)
-    return redirect(url_for('home_blueprint.index'))
+        return render_template('accounts/login.html', msg='Wrong user or password', form=login_form)
 
+    if not current_user.is_authenticated:
+        return render_template('accounts/login.html', form=login_form)
+    
+    return redirect(url_for('home_blueprint.index'))
 
 @blueprint.route('/register', methods=['GET', 'POST'])
 def register():
     create_account_form = CreateAccountForm(request.form)
     if 'register' in request.form:
-
         username = request.form['username']
         email = request.form['email']
-
-        # Check usename exists
         user = Users.query.filter_by(username=username).first()
-        if user:
-            return render_template('accounts/register.html',
-                                   msg='Username already registered',
-                                   success=False,
-                                   form=create_account_form)
 
-        # Check email exists
+        if user:
+            return render_template('accounts/register.html', msg='Username already registered', success=False, form=create_account_form)
+
         user = Users.query.filter_by(email=email).first()
+        
         if user:
-            return render_template('accounts/register.html',
-                                   msg='Email already registered',
-                                   success=False,
-                                   form=create_account_form)
+            return render_template('accounts/register.html', msg='Email already registered', success=False, form=create_account_form)
 
-        # else we can create the user
         user = Users(**request.form)
         db.session.add(user)
         db.session.commit()
-
-        # Delete user from session
         logout_user()
 
-        return render_template('accounts/register.html',
-                               msg='User created successfully.',
-                               success=True,
-                               form=create_account_form)
-
+        return render_template('accounts/register.html', msg='User created successfully.', success=True, form=create_account_form)
     else:
         return render_template('accounts/register.html', form=create_account_form)
-
 
 @blueprint.route('/logout')
 def logout():
@@ -141,42 +114,28 @@ def logout():
 def unauthorized_handler():
     return render_template('home/page-403.html'), 403
 
-
 @blueprint.errorhandler(403)
 def access_forbidden(error):
     return render_template('home/page-403.html'), 403
-
 
 @blueprint.errorhandler(404)
 def not_found_error(error):
     return render_template('home/page-404.html'), 404
 
-
 @blueprint.errorhandler(500)
 def internal_error(error):
     return render_template('home/page-500.html'), 500
 
-async def process_task(value):
-    # value = request.form.get('value')
-    reqUrl = "https://gate-001.up.railway.app/runserver/"
-    # reqUrl = random.choice([
-    #                         #"https://8585-vogler-freegamesclaimer-ko3sbr85ysc.ws-us105.gitpod.io/runserver/", 
-    #                         #"https://8585-gerry002023-bincheck-5cbzxb29b48.ws-us105.gitpod.io/runserver/",
-    #                         "https://67nj73ce138tyd-8585.proxy.runpod.net/runserver/",
-    #                         "https://h3y34ex2oxf1aa-8585.proxy.runpod.net/runserver/"
-    #                        ])
-    
+async def process_task(value, gates):
+    reqUrl = f"https://{gates}/runserver/"
     headersList = {
-    "Accept": "*/*",
-    "User-Agent": "Thunder Client (https://www.thunderclient.com)",
-    "Content-Type": "application/json" 
+        "Accept": "*/*",
+        "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+        "Content-Type": "application/json" 
     }
-    
-    payload = json.dumps({"userinfo": "your_user_info_here",
-                          "remarks": "your_remarks_here",
-                          "card": value})
-    response = requests.request("POST", reqUrl, data=payload,  headers=headersList)
-    time.sleep(1)
+    payload = json.dumps({"userinfo": "your_user_info_here", "remarks": "your_remarks_here", "card": value})
+    response = requests.post(reqUrl, data=payload, headers=headersList)
+    await asyncio.sleep(1)
     message = find_between(response.text, '"message":"', '"')
     print(message)
     return value, message
@@ -184,45 +143,21 @@ async def process_task(value):
 async def worker(queue, results):
     while True:
         value = await queue.get()
-        result = await process_task(value)
+        gates = get_rotation_gates()
+        print(gates)
+        result = await process_task(value, gates)
         results.append(result) 
         queue.task_done()
-        
-@blueprint.route('/gate1', methods=['POST'])
-async def gate1():
+
+@blueprint.route('/gate<int:gate_number>', methods=['POST'])
+async def gate(gate_number):
     value = request.form.get('value')
     queue = asyncio.Queue()
     queue.put_nowait(value)
     results = []
     worker_task = asyncio.create_task(worker(queue, results))
-    await queue.join()
+    await asyncio.gather(queue.join(), worker_task)
+    response = ''
     for result in results:
-        response = f"{result[0]} => {result[1]}"
+        response += f"{result[0]} => {result[1]}\n"
     return response
-
-    
-@blueprint.route('/gate2', methods=['POST'])
-async def gate2():
-    value = request.form.get('value')
-    queue = asyncio.Queue()
-    queue.put_nowait(value)
-    results = []
-    worker_task = asyncio.create_task(worker(queue, results))
-    await queue.join()
-    for result in results:
-        response = f"{result[0]} => {result[1]}"
-    return response
-
-   
-@blueprint.route('/gate3', methods=['POST'])
-async def gate3():
-    value = request.form.get('value')
-    queue = asyncio.Queue()
-    queue.put_nowait(value)
-    results = []
-    worker_task = asyncio.create_task(worker(queue, results))
-    await queue.join()
-    for result in results:
-        response = f"{result[0]} => {result[1]}"
-    return response
-
